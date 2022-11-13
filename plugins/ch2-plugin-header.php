308f56a712ba3cf624957f032ca29561c8ff6bdc
mauro bono <bono.mauro@gmail.com>
2022-11-13 16:08:01 +0100

    [add] ch2-plugin-header the very basic of plugin development: the header

---
+++ plugins/ch2-plugin-header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Simple Plugin Header
+ * Plugin URI:
+ * Description: Create a plugin visible in WordPress Admin interface
+ * Version: 1.0
+ * Author: Mauro Bono
+ * Author URI: https://maurobono.com
+ * License: GPLv2
+*/
