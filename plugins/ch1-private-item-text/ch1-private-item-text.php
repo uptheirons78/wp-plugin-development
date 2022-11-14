329c5178a5e1fcaca8aba9f4170821422603a093
mauro bono <bono.mauro@gmail.com>
2022-11-14 18:48:32 +0100

    [add] styles to private text item shortcode

--- plugins/ch1-private-item-text/ch1-private-item-text.php
+++ plugins/ch1-private-item-text/ch1-private-item-text.php
@@ -13,12 +13,19 @@ add_shortcode( 'private', 'ch1_private_shortcode' );
 function ch1_private_shortcode( $atts, $content = null )
 {
   if ( is_user_logged_in() ) {
-    return '<div class="private">' . $content . '</div>';
+    return '<div class="pit-private">' . $content . '</div>';
   } else {
-    $output = '<div class="register">';
+    $output = '<div class="pit-register">';
     $output .= 'You need to become a member to access this content.';
     $output .= '</div>';
 
     return $output;
   }
+}
+
+add_action( 'wp_enqueue_scripts', 'ch1_pit_enqueue_styles');
+
+function ch1_pit_enqueue_styles()
+{
+  wp_enqueue_style( 'private_shortcode_styles', plugins_url( 'style.css', __FILE__ ) );
 }
