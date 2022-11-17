3d0a8f5f7b8e4d5691bc851f7150f86e877857a5
mauro bono <bono.mauro@gmail.com>
2022-11-17 16:15:13 +0100

    [update] ch2-multi-level-menu plugin so hooks can be loaded only when in admin pages

--- plugins/ch2-multi-level-menu/ch2-multi-level-menu.php
+++ plugins/ch2-multi-level-menu/ch2-multi-level-menu.php
@@ -8,30 +8,9 @@
  * Author URI: https://maurobono.com
  * License: GPLv2
  */
-add_action( 'admin_menu', 'ch2_mlm_admin_menu' );
+define('ch2mlm', 1);
+define( 'MLM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-function ch2_mlm_admin_menu()
-{
-  // SVG icon for the menu (instead of Dashicons)
-  $menu_icon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" /></svg>';
-
-  // Create the top level menu
-  add_menu_page(
-    'My Complex Plugin Configuration Page',
-    'My Complex Plugin',
-    'manage_options',
-    'ch2-mlm-main-menu',
-    'ch2_mlm_complex_main',
-    'data:image/svg+xml;base64,' . base64_encode( $menu_icon )
-  );
-
-  // Create a sub-menu under the top-level one
-  add_submenu_page(
-    'ch2-mlm-main-menu',
-    'My Complex Menu Sub-config Page',
-    'Sub-Config Page',
-    'manage_options',
-    'ch2-mlm-sub-menu',
-    'ch2_my_complex_submenu'
-  );
+if (is_admin()) {
+  require_once MLM_PLUGIN_DIR . 'ch2-multi-level-menu-admin-functions.php';
 }
