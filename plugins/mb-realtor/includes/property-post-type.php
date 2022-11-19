8c4629c747591a7d86d185afc58046d30abd76a6
mauro bono <bono.mauro@gmail.com>
2022-11-19 11:06:45 +0100

    [add] mb-realtor

---
+++ plugins/mb-realtor/includes/property-post-type.php
@@ -0,0 +1,41 @@
+<?php
+
+add_action( 'init', 'mbr_register_property_post_type' );
+
+function mbr_register_property_post_type()
+{
+
+  $labels = array(
+    'name'               => esc_html__('Properties', 'mb-realtor'),
+    'singular_name'      => esc_html__('Property', 'mb-realtor'),
+    'add_new'            => esc_html__('Add New', 'mb-realtor'),
+    'add_new_item'       => esc_html__('Add New Property', 'mb-realtor'),
+    'edit'               => esc_html__('Edit', 'mb-realtor'),
+    'edit_item'          => esc_html__('Edit Property', 'mb-realtor'),
+    'new_item'           => esc_html__('New Property', 'mb-realtor'),
+    'view'               => esc_html__('View', 'mb-realtor'),
+    'view_item'          => esc_html__('View Property', 'mb-realtor'),
+    'search_items'       => esc_html__('Search Properties', 'mb-realtor'),
+    'not_found'          => esc_html__('No Properties Found', 'mb-realtor'),
+    'not_found_in_trash' => esc_html__('No Properties Found in trash', 'mb-realtor'),
+    'parent'             => esc_html__('Parent Property', 'mb-realtor'),
+    'menu_name'          => esc_html__('Properties', 'mb-realtor'),
+    'name_admin_bar'     => esc_html__('Property', 'mb-realtor'),
+    'update_item'        => esc_html__('View Property', 'mb-realtor'),
+    'all_items'          => esc_html__('All Properties', 'mb-realtor'),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'menu_position' => 20,
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+    'taxonomies' => array( '' ),
+    'menu_icon' => 'dashicons-admin-home',
+    'has_archive' => false,
+    'exclude_from_search' => true,
+    'rewrite' => array( 'slug' => 'properties', 'with_front' => false),
+  );
+
+  register_post_type( 'property', $args );
+}
---
+++ plugins/mb-realtor/mb-realtor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: MB Realtor
+ * Plugin URI: https://maurobono.com
+ * Description: This plugin add a 'Property' custom post type to your WordPress website and allows Realtors or Real Estate * Agents to show and list their properties portfolio.
+ * Version: 1.0
+ * Author: Mauro Bono
+ * Author URI: https://maurobono.com
+ * Text Domain: mb-realtor
+ * License: GPLv2
+ */
+
+define( 'MBREALTOR_VERSION', '1.0' );
+
+define( 'MBREALTOR_PLUGIN', __FILE__ );
+
+define( 'MBREALTOR_PLUGIN_BASENAME', plugin_basename( MBREALTOR_PLUGIN ) );
+
+define( 'MBREALTOR_PLUGIN_NAME', trim( dirname( MBREALTOR_PLUGIN_BASENAME ), '/' ) );
+
+define( 'MBREALTOR_PLUGIN_DIR', untrailingslashit( dirname( MBREALTOR_PLUGIN ) ) );
+
+include_once MBREALTOR_PLUGIN_DIR . '/includes/property-post-type.php';
